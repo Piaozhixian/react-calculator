a7a06d5604dc52657ace00b1acfbdf6c24d01275
Piao <pakushiken@gmail.com>
2021-10-10 04:53:52 +0900

    set panel

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -8,12 +9,79 @@ function App() {
           Calculator
         </p>
       </header>
-      <body>
-        <div className="calculator">
+       <div className="calculator">
+         <Board/>
         </div>
-      </body>
     </div>
   );
+}
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="display-panel"></div>
+        <div className="button-panel">
+          <div className="button-row">
+            <div className="button">
+              <p>1</p>
+            </div>
+            <div className="button">
+              <p>2</p>
+            </div>
+            <div className="button">
+              <p>3</p>
+            </div>
+            <div className="button">
+              <p>+</p>
+            </div>
+          </div>
+          <div className="button-row">
+            <div className="button">
+              <p>4</p>
+            </div>
+            <div className="button">
+              <p>5</p>
+            </div>
+            <div className="button">
+              <p>6</p>
+            </div>
+            <div className="button">
+              <p>-</p>
+            </div>
+          </div>
+          <div className="button-row">
+            <div className="button">
+              <p>7</p>
+            </div>
+            <div className="button">
+              <p>8</p>
+            </div>
+            <div className="button">
+              <p>9</p>
+            </div>
+            <div className="button">
+              <p>*</p>
+            </div>
+          </div>
+          <div className="button-row">
+            <div className="button">
+              <p>0</p>
+            </div>
+            <div className="button">
+              <p>←</p>
+            </div>
+            <div className="button">
+              <p>=</p>
+            </div>
+            <div className="button">
+              <p>/</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
